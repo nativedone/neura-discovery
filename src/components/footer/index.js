26307dbb1e2261de9b23adf37ac89bf40c9dbe27
barrosdvlpr <barrosdvlpr@gmail.com>
2022-04-27 17:56:48 -0300

    working on footer styles

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,4 @@
 import { styled } from "@theme";
-// import Image from 'next/image';
 
 import { InstagramIcon } from './instagramIcon';
 import { YoutubeIcon } from './youtubeIcon';
@@ -10,28 +9,23 @@ import { LogoNew } from './logoNew';
 
 // TODO: fix my styles and content
 
-
 export function Footer() {
   return (
     <SemanticFooter>
       <SemanticContainer>
         <ExternalLinksNav>
-          <span>©2022 NeuRA - All Rights Reserved DISCLAIMER</span>
-          <span>DISCLAIMER | PRIVACY | CONTACT US | ABOUT US</span>
+          <Rights><span>©2022 NeuRA - All Rights Reserved</span></Rights>
+          <Links><span>DISCLAIMER | PRIVACY | CONTACT US | ABOUT US</span></Links>
         </ExternalLinksNav>
-
         <SocialLinksNav>
           <LogoControl>
             <LogoNew/>
             {/* <LogoIcon/> */}
           </LogoControl>
           <Span> 
-            <FacebookIcon/> 
-              {/* <Space>|</Space> */}           
+            <FacebookIcon/>           
             <InstagramIcon/>
-              {/* <Space>|</Space> */}
             <InstagramIcon/>
-              {/* <Space>|</Space> */}
             {/* <TwitterIcon/> */} 
             <YoutubeIcon/>       
           </Span>
@@ -45,7 +39,6 @@ const SemanticFooter = styled("footer", {
   display: "flex",
   justifyContent:"center",
   backgroundColor: "#fff",
-
   zIndex: "$50",
   padding: "$3x_2 0",
 });
@@ -53,13 +46,27 @@ const SemanticFooter = styled("footer", {
 const LogoControl = styled("div",{
   /* 4.25rem(68px) @ 20rem(320px) increasing to 10.5rem(168px) @ 160rem(2560px) */
   fontSize: "clamp(4.25rem, calc(4.25rem + ((1vw - 0.2rem) * 4.4643)), 10.5rem)",
-
   /* Safari resize fix */
   minHeight: "0vw",
+
+  display: "flex",
+  textAlign:"center",
+  alignItens:"center",
+  justifyContent:"center",
+  marginTop:"1rem",
+  marginBottom:"1rem",
+  "@3": {
+    marginBottom:"3rem",
+    // display:"none",
+  }
+
 });
 
 const SemanticContainer = styled("div", {
-  width: "45vw",
+  width: "60vw",
+  "@3": { 
+    width: "45vw",
+  }
 });
 
 const ExternalLinksNav = styled("nav", {
@@ -68,13 +75,9 @@ const ExternalLinksNav = styled("nav", {
   textAlign:"center",
 });
 
-const SocialLinksNav = styled("nav", {
-    
+const SocialLinksNav = styled("nav", {   
   display: "flex",
   flexDirection: "column",
-  // alignItens: "center",
-  // justifyContent:"center",
-  textAlign:"center",
 });
 
 const Span = styled("span", {
@@ -87,8 +90,13 @@ const Span = styled("span", {
 
 });
 
-// const Space = styled("div", {
-//   display: "flex",
-//   justifyContent:"center",
-//   alignItens: "center",
-// });
+const Rights = styled("div",{
+  paddingBottom:"10px",
+})
+
+const Links = styled("div",{
+  paddingBottom:".5rem",
+  "@3": { 
+  paddingBottom:"3rem",
+  }
+})
