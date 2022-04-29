7be9bdc0c6c754b0da871d1f75dc4d9addd202ed
barrosdvlpr <barrosdvlpr@gmail.com>
2022-04-29 11:27:25 -0300

    fix images position on swiper at 320px

--- src/feature/home-page/carousel-section/swiper.js
+++ src/feature/home-page/carousel-section/swiper.js
@@ -140,11 +140,12 @@ export function CarouselSwiper() {
       <CarouselDesktopContainer>
         <Swiper
           ref={swiperRef}
-          cssMode={true}
+          // cssMode={true}
           // navigation={true}
+          speed= {1200}
           slidesPerView={1}
           // pagination={true}
-          mousewheel={true}
+          // mousewheel={true}
           keyboard={true}
           modules={[Navigation, Pagination, Mousewheel, Keyboard]}
           // className="mySwiper"
@@ -241,10 +242,13 @@ const CarouselParagraph = styled("p", {
 
 const ImageContainer = styled("div", {
   display: "flex",
-  width: "100%",
-  height: "100%",
+  justifyContent:"center",
+  width: "100vw",
+  // height: "calc( (235 / 320) * 100vw )",
   // aspectRatio: "1",
   "@3": {
+    justifyContent:"initial",
+    height: "calc( (998 / 2574) * 100vw )",
     position: "absolute",
     top:0,
     left:0,
@@ -256,7 +260,7 @@ const ImageContainer = styled("div", {
 const TextContainer = styled("div", {
   display: "flex",
   flexDirection: "column",
-  // justifyContent: "center",
+  justifyContent: "center",
   // alignItens:"center",
   textAlign:"center",
   width: "85vw",
@@ -269,255 +273,26 @@ const TextContainer = styled("div", {
   // paddingRight:"0",
   backgroundColor: "#fff",
   paddingTop:".5rem",
-  paddingBottom: "2.2rem",
+  paddingBottom: "3.8rem",
   "@3": {
-    width: "40%",
+    width: "50%",
     zIndex:"100",
-    background: "-webkit-linear-gradient(0deg, rgba(255, 255, 255, 0),  rgba(255, 255, 255, 0.9) 50%, rgba(255, 255, 255, 1) 70%)",
-    // paddingTop:"$3x",
-    // paddingTop:"220px", // 2560
-    // paddingTop:"0px", // 1280
     textAlign:"left",
-    paddingTop:"clamp(1px, calc(0.0625rem + ((1vw - 10.1px) * 14.129)), 220px)", // 1px at 1010 - 240px at 2560
+    background: "-webkit-linear-gradient(0deg, rgba(255, 255, 255, 0),  rgba(255, 255, 255, 0.9) 50%, rgba(255, 255, 255, 1) 70%)",
+    
+    // paddingTop:"$3x",
+    // paddingTop:"90px", // 2560
+    // paddingTop:"0px", // 1280
+    // paddingTop:"clamp(1px, calc(0.0625rem + ((1vw - 10.1px) * 14.129)), 220px)", // 1px at 1010 - 240px at 2560 - test image
+    paddingTop:"clamp(0.0625rem, calc(0.0625rem + ((1vw - 0.5rem) * 5.0568)), 5.625rem)", // 1px at 800 - 90px at 2560
+    
     // paddingRight: "$3x",
-    // paddingRight: "290px", // 2560
-    // paddingRight: "30px", // 2560
-    paddingRight: "clamp(1.875rem, calc(1.875rem + ((1vw - 0.63125rem) * 16.7742)), 18.125rem)", // 30px at 1010 - 290px at 2560
+    // paddingRight: "580px", // 2560
+    // paddingRight: "120px", // 1280
+    // paddingRight: "0px", // 800
+    // paddingRight: "clamp(1.875rem, calc(1.875rem + ((1vw - 0.63125rem) * 16.7742)), 18.125rem)", // 30px at 1010 - 290px at 2560 - test image
+    paddingRight: "clamp(0.0625rem, calc(0.0625rem + ((1vw - 0.5rem) * 32.8977)), 36.25rem)", // 1px at 800 - 580 at 2560
     paddingLeft:"0px",
   },  
 });
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useRef, useState } from "react";
-// import { styled } from "@theme";
-
-// // import { CarouselItem } from "./carouselItem"
-
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
-
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-
-
-// // import required modules
-// import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
-
-
-
-// export function CarouselSwiper(props) {
-//   return (
-//     <>
-//       <Swiper
-//         cssMode={true}
-//         navigation={true}
-//         pagination={true}
-//         mousewheel={true}
-//         keyboard={true}
-//         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-//         className="mySwiper"
-//       >
-//         <SwiperSlide>
-//             <CarouselItem 
-//                 subheading={data[0].subheading}
-//                 paragraph={data[0].paragraph}
-//             />
-//         </SwiperSlide>
-//       </Swiper>
-//     </>
-//   );
-// }
-
-// const CarouselItem = ({ subheading, paragraph }) => {
-//   return (
-//     <CarouselItemContainer>
-//       <CarouselSubheading>{subheading}</CarouselSubheading>
-//       <CarouselParagraph>{paragraph}</CarouselParagraph>
-//     </CarouselItemContainer>
-//   );
-// };
-
-// const CarouselItemContainer = styled("div", {});
-
-// const CarouselSubheading = styled("span", {
-//   zIndex: "$40",
-//   fontSize: "$5",
-//   fontWeight: "$bold",
-//   background:
-//     "-webkit-linear-gradient(0deg, var(--color-red-left), var(--color-red-middle) 50%, var(--color-red-right) 100%)",
-//   WebkitBackgroundClip: "text",
-//   WebkitTextFillColor: "transparent",
-// });
-// const CarouselParagraph = styled("p", {
-//   zIndex: "$40",
-//   fontSize: "$0",
-//   lineHeight: "$tight",
-//   fontWeight: "$normal",
-// });
-
-// const data = [
-//   {
-//     key: "carousel-item1",
-//     subheading: "The Brain Atlas",
-//     paragraph: `Like all explorers, neuroscientists need maps to guide their path to success. World-renowned brain cartographer and NeuRA scientist Professor George Paxinos AO made medical history in 2019 when he launched the most comprehensive atlas of the human brainstem ever made. His work has assisted in the development of treatments for conditions like Parkinson’s and Alzheimer’s disease and is now considered the foundation for all neuroscientific research.`,
-//   },
-//   {
-//     key: "carousel-item2",
-//     subheading: "Connecting young & old",
-//     paragraph: `Inspired by the ABC’s Old People’s Home for 4 Year Olds, NeuRA scientists launched an intergenerational integration study that placed older Australians with pre-schoolers to see what physical, cognitive and social benefits could be brought to both groups. Early research suggests that coming together for purposeful activities could reduce frailty and feelings of loneliness in older people, while boosting social and language skills for children.`,
-//   },
-//   {
-//     key: "carousel-item3",
-//     subheading: "Sydney Brain Bank",
-//     paragraph: `Can you imagine a bank that houses not money, but human brain and spinal cord tissue? Well, the Sydney Brain Bank does exactly that. This NeuRA facility collects, stores and distributes tissue samples for research into conditions such as Alzheimer’s, Parkinson’s and motor neuron disease. The bank currently holds brain tissue from over 700 donors and donates around 6,000 specimens every year to research groups here and overseas to facilitate advances in medical research.`,
-//   },
-
-//   {
-//     key: "carousel-item4",
-//     subheading: "The MUGgLE Study",
-//     paragraph: `In the world of Harry Potter, Muggles didn’t have any magical abilities. But at NeuRA, MUGgLEs have very special powers — they help researchers understand more about how muscles grow and develop in children with cerebral palsy. More than 34,000 Australians live with this condition, which has no known cure. Data from the 320 participants aged 5–14 will help researchers track muscles over time, creating a vital foundation for further investigation. Pretty magical stuff!`,
-//   },
-//   {
-//     key: "carousel-item5",
-//     subheading: "Supporting schizophrenia",
-//     paragraph: `One in 100 people live with schizophrenia, a serious mental disorder that causes people to misinterpret reality. Current treatments only partially address psychotic symptoms and don’t restore people to their full potential. NeuRA’s Schizophrenia Research Institute is the only national institute solely dedicated to discovering ways to treat, prevent and cure this disorder and in 2020, our scientists made a discovery that opened a new avenue for more effective treatment.`,
-//   },
-// ];
-
-
-
-
-
-
-// import React, { useRef, useState } from "react";
-// import { styled } from "@theme";
-
-// // import { CarouselItem } from "./carouselItem"
-
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
-
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-
-
-// // import required modules
-// import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
-
-// const data = [
-//     {
-//         id: "carousel-item1",
-//       subheading: "The Brain Atlas",
-//       paragraph: `Like all explorers, neuroscientists need maps to guide their path to success. World-renowned brain cartographer and NeuRA scientist Professor George Paxinos AO made medical history in 2019 when he launched the most comprehensive atlas of the human brainstem ever made. His work has assisted in the development of treatments for conditions like Parkinson’s and Alzheimer’s disease and is now considered the foundation for all neuroscientific research.`,
-//     },
-//     {
-//         id: "carousel-item2",
-//       subheading: "Connecting young & old",
-//       paragraph: `Inspired by the ABC’s Old People’s Home for 4 Year Olds, NeuRA scientists launched an intergenerational integration study that placed older Australians with pre-schoolers to see what physical, cognitive and social benefits could be brought to both groups. Early research suggests that coming together for purposeful activities could reduce frailty and feelings of loneliness in older people, while boosting social and language skills for children.`,
-//     },
-//     {
-//         id: "carousel-item3",
-//       subheading: "Sydney Brain Bank",
-//       paragraph: `Can you imagine a bank that houses not money, but human brain and spinal cord tissue? Well, the Sydney Brain Bank does exactly that. This NeuRA facility collects, stores and distributes tissue samples for research into conditions such as Alzheimer’s, Parkinson’s and motor neuron disease. The bank currently holds brain tissue from over 700 donors and donates around 6,000 specimens every year to research groups here and overseas to facilitate advances in medical research.`,
-//     },
-  
-//     {
-//         id: "carousel-item4",
-//       subheading: "The MUGgLE Study",
-//       paragraph: `In the world of Harry Potter, Muggles didn’t have any magical abilities. But at NeuRA, MUGgLEs have very special powers — they help researchers understand more about how muscles grow and develop in children with cerebral palsy. More than 34,000 Australians live with this condition, which has no known cure. Data from the 320 participants aged 5–14 will help researchers track muscles over time, creating a vital foundation for further investigation. Pretty magical stuff!`,
-//     },
-//     {
-//         id: "carousel-item5",
-//       subheading: "Supporting schizophrenia",
-//       paragraph: `One in 100 people live with schizophrenia, a serious mental disorder that causes people to misinterpret reality. Current treatments only partially address psychotic symptoms and don’t restore people to their full potential. NeuRA’s Schizophrenia Research Institute is the only national institute solely dedicated to discovering ways to treat, prevent and cure this disorder and in 2020, our scientists made a discovery that opened a new avenue for more effective treatment.`,
-//     },
-// ];
-
-// // const CarouselItemContainer = styled("div", {});
-
-
-
-// const CarouselSubheading = styled("span", {
-//   zIndex: "$40",
-//   fontSize: "$5",
-//   fontWeight: "$bold",
-//   background:
-//     "-webkit-linear-gradient(0deg, var(--color-red-left), var(--color-red-middle) 50%, var(--color-red-right) 100%)",
-//   WebkitBackgroundClip: "text",
-//   WebkitTextFillColor: "transparent",
-// });
-// const CarouselParagraph = styled("p", {
-//   zIndex: "$40",
-//   fontSize: "$0",
-//   lineHeight: "$tight",
-//   fontWeight: "$normal",
-// });
-
-
-// // const CarouselItem = ({ subheading, paragraph, key }) => {
-// //     return (
-// //       <div key={key}>
-// //         <CarouselSubheading>{subheading}</CarouselSubheading>
-// //         <CarouselParagraph>{paragraph}</CarouselParagraph>
-// //       </div>
-// //     );
-// // }
-
-// class CarouselItem extends React.Component {
-//     render() {
-//     const rendereData = data.map(({subheading, paragraph, id}) => {
-//         return (
-//             <div key={id}>
-//                 {subheading}
-//                 {paragraph}
-//             </div>
-//         )
-//         });
-
-//         return (
-//             <div>
-//                 {rendereData}
-//             </div>
-//         );
-//     }
-// }
-  
-
-// export function CarouselSwiper() {
-//   return (
-//     <>
-//       <Swiper
-//         cssMode={true}
-//         navigation={true}
-//         pagination={true}
-//         mousewheel={true}
-//         keyboard={true}
-//         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-//         className="mySwiper"
-//       >
-//         <SwiperSlide>
-//             <CarouselItem 
-//                 // subheading={data.subheading}
-//                 // paragraph={data.paragraph}
-//             />
-//         </SwiperSlide>
-//       </Swiper>
-//     </>
-//   );
-
-
-// }
 
