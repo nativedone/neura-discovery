d68eff94b1ae3e87c5d77250567cd0e13c16001d
nativedone <adeildoamorim@gmail.com>
2022-05-23 20:02:20 -0300

    there's no route change in this project

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Script from "next/script";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 import * as gtag from "@lib/gtm";
 import "@styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageView(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+  // const router = useRouter();
+  // useEffect(() => {
+  //   const handleRouteChange = (url) => {
+  //     gtag.pageView(url);
+  //   };
+  //   router.events.on("routeChangeComplete", handleRouteChange);
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, [router.events]);
 
   return (
     <>
