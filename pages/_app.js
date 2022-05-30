e4f99989c99813cf1eea8731d95043e5bbfd4b1c
nativedone <adeildoamorim@gmail.com>
2022-05-30 14:27:17 -0300

    add NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GTM_ID}`}
       />
       <Script
         id="gtag-init"
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
+            gtag('config', '${gtag.GTM_ID}', {
               page_path: window.location.pathname,
             });
           `,
