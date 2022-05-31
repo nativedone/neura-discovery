b4964fee1f6cfae99e93a0d648bfa0c2ecb5f8bc
nativedone <adeildoamorim@gmail.com>
2022-05-31 16:16:04 -0300

    Make donate page responsivel

--- pages/donate.js
+++ pages/donate.js
@@ -1,5 +1,6 @@
 import { SEO } from "@components/seo";
 import { Layout } from "@components/layout";
+import { styled } from "@theme";
 
 import { BackgroundVideo } from "../src/feature/home-page/background-video";
 
@@ -10,45 +11,69 @@ export default function DonateRoute() {
     <>
       <SEO />
       <Layout>
-        <div
-          className="body-block"
-          style={{
-            marginTop: "15vh",
-            marginBottom: "5vh",
-            width: "100vw",
-            zIndex: 999999,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div
-            id="blackbaud-donation-form_dac3f66e-18f7-4d4b-87f9-96a1a4761f4e"
-            style={{
-              width: "100vw",
-              zIndex: 999999,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          ></div>
+        <GivingFormContainer>
+          <GivingForm id="blackbaud-donation-form_dac3f66e-18f7-4d4b-87f9-96a1a4761f4e" />
           <script
             async
             src="https://sky.blackbaudcdn.net/static/donor-form-loader/1/main.js"
-          ></script>
+          />
+
           <Script
-            id="gtag-init-----dsfbgkj--ffgff"
+            id="id-BBDonorFormLoader"
             strategy="lazyOnload"
             dangerouslySetInnerHTML={{
               __html: `
-          window.BBDonorFormLoader = window.BBDonorFormLoader || [];
-            BBDonorFormLoader.newBlackbaudDonationFormZoned('renxt','p-s7Lu0uYOtU-B7WwWoPeu7A', 'dac3f66e-18f7-4d4b-87f9-96a1a4761f4e','aus');
+              window.BBDonorFormLoader = window.BBDonorFormLoader || {};
+              if(typeof  BBDonorFormLoader.newBlackbaudDonationFormZoned === 'function'){
+                BBDonorFormLoader.newBlackbaudDonationFormZoned('renxt','p-s7Lu0uYOtU-B7WwWoPeu7A', 'dac3f66e-18f7-4d4b-87f9-96a1a4761f4e','aus');
+              }
           `,
             }}
           />
-        </div>
+        </GivingFormContainer>
         <BackgroundVideo />
       </Layout>
     </>
   );
 }
+
+const GivingFormContainer = styled("div", {
+  marginTop: "15vh",
+  marginBottom: "5vh",
+  width: "100vw",
+  zIndex: 999999,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+
+  "> div": {
+   
+    minHeight: "80vh",
+    backgroundColor: 'white',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "90vw",
+    "@3": {
+      width: "42vw",
+    },
+  
+    "@8": {
+      width: "30vw",
+      minHeight: "65vh",
+      // backgroundColor: 'red',
+    },
+  
+    "@9": {
+      width: "26vw",
+      minHeight: "50vh",
+      // backgroundColor: 'blue',
+    },
+  
+    iframe: {
+      minWidth: '300px !important'
+    }
+  }
+});
+
+const GivingForm = styled("div", {});
