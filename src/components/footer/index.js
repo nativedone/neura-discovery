1b086bf2245491c2de76b46d1d728000f5786eeb
barrosdvlpr <barrosdvlpr@gmail.com>
2022-07-01 10:23:00 -0300

    Change footer logo image

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,8 @@ import { YoutubeIcon } from './youtubeIcon';
 import { TwitterIcon } from './twitterIcon';
 import { FacebookIcon } from './facebookIcon';
 import { VisuallyHidden } from "@components/visually-hidden";
-import { LogoNew } from './logoNew';
+
+import { Logo } from "../logo"
 
 export function Footer() {
   return (
@@ -45,23 +46,25 @@ export function Footer() {
               target="_blank"
               rel="noreferrer"
             >
-            <LogoNew />
+            <Logo />
             <VisuallyHidden>Visit our Webpage</VisuallyHidden>
             </a>
           </li>
         </LogoControl>
         <SocialLinksNav>
-          <Span>           
-            <li className="logo">
+          <Span>
+            <BottomLogoControl>             
+            <li>
               <a
                 href="https://www.neura.edu.au/"
                 target="_blank"
                 rel="noreferrer"
               >
-                <LogoNew />
+                <Logo />
                 <VisuallyHidden>Visit our Webpage</VisuallyHidden>
               </a>
-            </li>             
+            </li> 
+            </BottomLogoControl>            
             <li className="li">
               <a
                 href="https://www.facebook.com/NeuroscienceResearchAustralia/"
@@ -128,26 +131,64 @@ const SemanticFooter = styled("footer", {
 });
 
 const LogoControl = styled("ul",{
- /* 2.125rem(34px) @ 20rem(320px) increasing to 5.35rem(85.6px) @ 160rem(2560px) */
-  fontSize: "clamp(2.125rem, calc(2.125rem + ((1vw - 0.2rem) * 2.3036)), 5.35rem)",
+  width:
+  /* 6.881875rem(110.11px) @ 20rem(320px) increasing to 8.07125rem(129.14px) @ 35.9375rem(575px) */
+  "clamp(6.881875rem, calc(6.881875rem + ((1vw - 0.2rem) * 7.4627)), 8.07125rem)",
+
+  height: 
+  /* 2.125rem(34px) @ 20rem(320px) increasing to 2.49125rem(39.86px) @ 35.9375rem(575px) */
+  "clamp(2.125rem, calc(2.125rem + ((1vw - 0.2rem) * 2.298)), 2.49125rem)",
 
   /* Safari resize fix */
+  margin: "0 auto",
   minHeight: "0vw",
   display: "flex",
   textAlign:"center",
   alignItems: "center",
   justifyContent:"center",
-  marginTop:"1rem",
-  marginBottom:"1rem",
+
+  // marginTop:"17px", // 320
+  // marginTop:"18px", // 575
+  /* 1.0625rem(17px) @ 20rem(320px) increasing to 1.125rem(18px) @ 35.9375rem(575px) */
+  marginTop: "clamp(1.0625rem, calc(1.0625rem + ((1vw - 0.2rem) * 0.3922)), 1.125rem)",
+
+  // marginBottom:"28px", // 320
+  // marginBottom:"29px", // 575
+  /* 1.75rem(28px) @ 20rem(320px) increasing to 1.8125rem(29px) @ 35.9375rem(575px) */
+  marginBottom: "clamp(1.75rem, calc(1.75rem + ((1vw - 0.2rem) * 0.3922)), 1.8125rem)",
+
   ".li" : {
     listStyle: "none",
   },
   "@0":{
     display: "block"
-    
   },
   "@3": {
     display: "none"
+  }
+});
+
+const BottomLogoControl = styled("ul",{
+  /* 5.0635rem(81.016px) @ 35.9375rem(575px) increasing to 12.1445rem(194.312px) @ 160rem(2560px) */
+  width: "clamp(5.0635rem, calc(5.0635rem + ((1vw - 0.359375rem) * 5.7076)), 12.1445rem)",
+
+  /* 2.1494375rem(34.391px) @ 35.9375rem(575px) increasing to 5.15625rem(82.5px) @ 160rem(2560px) */
+  height: "clamp(2.1494375rem, calc(2.1494375rem + ((1vw - 0.359375rem) * 2.4236)), 5.15625rem)",
+
+  /* 0.0625rem(1px) @ 35.9375rem(575px) increasing to 0.1875rem(3px) @ 160rem(2560px) */
+  marginTop: "clamp(0.0625rem, calc(0.0625rem + ((1vw - 0.359375rem) * 0.1008)), 0.1875rem)",
+
+  minHeight: "0vw",
+
+  ".li" : {
+    listStyle: "none",
+  },
+  "@0":{
+    display: "none"
+    
+  },
+  "@3": {
+    display: "block"
   }
 });
 
@@ -195,17 +236,6 @@ const Span = styled("ul", {
 
 ".li" : {
   listStyle: "none",
-},
-
-".logo" : {
-  listStyle: "none",
-
-  "@0":{
-    display: "none"
-  },
-  "@3": {
-    display: "block"
-  }
 },
 });
 
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -13,7 +13,8 @@ const headerAnimationVariants = {
   hide: { opacity: 0, y: "-100%" },
 };
 
-import { Logo } from "./logo";
+// import { Logo } from "./logo";
+import { Logo } from "../logo";
 
 export function Header({ animateOnScroll = true }) {
   const { scrollY } = useViewportScroll();
