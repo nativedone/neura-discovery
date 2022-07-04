67d9c20bc1e66a031468617365a79f79237d4d42
nativedone <adeildoamorim@gmail.com>
2022-07-04 14:47:45 -0300

    optmize logo image priority & placeholder

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -5,11 +5,14 @@ import Image from "next/image";
 export const Logo = () => {
   return (
     <Image
-      src="/assets/h100_300dpi.webp"
+      src="/assets/h50_300dpi.webp"
       layout="responsive"
       alt="neura logo"
       width="2021"
       height="624"
+      priority
+      placeholder="blur"
+      blurDataURL="/assets/h50_300dpi-blur-w50-q10-b10.webp"
     />
   );
 };
